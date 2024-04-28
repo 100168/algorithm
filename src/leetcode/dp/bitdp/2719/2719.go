5a27f8c807a68cd9126ce7d5ce6788bd11bdb432
jian.pan <jian.pan@jiduauto.com>
2024-04-28 19:14:29 +0800

    好样的

--- src/leetcode/dp/bitdp/2719/2719.go
+++ src/leetcode/dp/bitdp/2719/2719.go
@@ -10,14 +10,60 @@ min_sum <= digit_sum(x) <= max_sum.
 
 注意，digit_sum(x) 表示 x 各位数字之和。
 */
-//func count(num1 string, num2 string, min_sum int, max_sum int) int {
-//
-//	mod := int(1e9 + 7)
-//
-//	var dfs func(int, int, int, int) int
-//
-//	dfs = func(index int, pre int, isLimit int, isNum int) int {
-//
-//	}
-//
-//}
+func count(num1 string, num2 string, min_sum int, max_sum int) int {
+
+	mod := int(1e9 + 7)
+
+	n := len(num2)
+	diff := n - len(num1)
+
+	memo := make([]map[int]int, n)
+
+	for i := range memo {
+		memo[i] = make(map[int]int)
+	}
+	var dfs func(int, bool, bool, int) int
+
+	dfs = func(i int, limitLow bool, limitUp bool, sum int) int {
+		if i == n {
+			if min_sum <= sum && sum <= max_sum {
+				return 1
+			}
+			return 0
+		}
+
+		if !limitLow && !limitUp {
+
+			if _, ok := memo[i][sum]; ok {
+				return memo[i][sum]
+			}
+		}
+
+		cur := 0
+		low := 0
+		up := 9
+
+		if limitLow && i >= diff {
+			low = int(num1[i-diff] - '0')
+		}
+
+		if limitUp {
+			up = int(num2[i] - '0')
+		}
+
+		for d := low; d <= up; d++ {
+			cur += dfs(i+1, limitLow && d == low, limitUp && d == up, sum+d)
+		}
+
+		cur %= mod
+		if !limitLow && !limitUp {
+
+			memo[i][sum] = cur
+
+		}
+
+		return cur
+	}
+
+	return dfs(0, true, true, 0)
+}
--- src/leetcode/dp/bitdp/2827/a_2827.go
+++ src/leetcode/dp/bitdp/2827/a_2827.go
@@ -1,6 +1,9 @@
 package main
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 // 给你正整数 low ，high 和 k 。
 //
@@ -10,107 +13,77 @@ import "strconv"
 // 这个整数可以被 k 整除。
 // 请你返回范围 [low, high] 中美丽整数的数目。
 func numberOfBeautifulIntegers(low, high, k int) int {
-	calc := func(high int) int {
-		s := strconv.Itoa(high)
-		n := len(s)
-		memo := make([][][]int, n)
-		for i := range memo {
-			memo[i] = make([][]int, k+1)
-			for j := range memo[i] {
-				memo[i][j] = make([]int, n*2+1)
-				for k := range memo[i][j] {
-					memo[i][j][k] = -1 // -1 表示没有计算过
-				}
-			}
-		}
-		var dfs func(int, int, int, bool, bool) int
-		dfs = func(i, val, diff int, isLimit, isNum bool) (res int) {
-			if i == n {
-				if isNum && val == 0 && diff == n {
-					return 1 // 找到了一个合法数字
-				}
-				return 0
-			}
-			if !isLimit && isNum {
-				p := &memo[i][val][diff]
-				if *p >= 0 {
-					return *p
-				}
-				defer func() { *p = res }() // cache
-			}
-			if !isNum { // 可以跳过当前数位
-				res += dfs(i+1, val, diff, false, false)
-			}
-			up := 9
-			if isLimit {
-				up = int(s[i] - '0') // 如果前面填的数字都和 high 的一样，那么这一位至多填数字 s[i]（否则就超过 high 啦）
-			}
-			d := 0
-			if !isNum {
-				d = 1 // 如果前面没有填数字，必须从 1 开始（因为不能有前导零）
-			}
-			for ; d <= up; d++ { // 枚举要填入的数字 d
-				res += dfs(i+1, (val*10+d)%k, diff+d%2*2-1, isLimit && d == up, true)
-			}
-			return
-		}
-		return dfs(0, 0, n, true, false)
-	}
-	return calc(high) - calc(low-1)
-}
 
-func numberOfBeautifulIntegers2(low int, high int, k int) int {
-	return cal(high, k) - cal(low-1, k)
-}
+	lows := strconv.Itoa(low)
+	highs := strconv.Itoa(high)
 
-func cal(high, k int) int {
+	n1 := len(highs)
+	n2 := len(lows)
 
-	s := strconv.Itoa(high)
-	n := len(s)
-	memo := make([][][]int, n)
+	padding := n1 - n2
+	//填0对齐
+	lows = strings.Repeat("0", padding) + lows
+
+	memo := make([][]map[int]int, n1)
+
 	for i := range memo {
-		memo[i] = make([][]int, k)
+		memo[i] = make([]map[int]int, k)
 		for j := range memo[i] {
-			memo[i][j] = make([]int, 2*n+1)
-			for k := range memo[i][j] {
-				memo[i][j][k] = -1
-			}
+			memo[i][j] = make(map[int]int)
 		}
 	}
-	return dfs(0, 0, n, true, false, k, s, memo)
+
+	var dfs func(int, int, int, bool, bool, bool) int
+
+	dfs = func(i int, diff int, rest int, isLimitL bool, isLimitH bool, isNum bool) int {
+
+		if i == n1 {
+			if diff == 0 && rest == 0 {
+				return 1
+			}
+			return 0
+		}
+
+		if _, ok := memo[i][rest][diff]; ok && !isLimitL && !isLimitH && isNum {
+			return memo[i][rest][diff]
+		}
+
+		//不选当前数
+		res := 0
+		if !isNum && i < padding {
+			res += dfs(i+1, diff, rest, true, false, false)
+		}
+
+		low := 0
+		up := 9
+
+		down := 0
+		if !isNum {
+			down = 1
+		}
+		if isLimitL && i >= padding {
+			low = int(lows[i] - '0')
+		}
+		if isLimitH {
+			up = int(highs[i] - '0')
+		}
+
+		for d := max(low, down); d <= up; d++ {
+			res += dfs(i+1, diff+(d%2)*2-1, (rest*10+d)%k, isLimitL && d == low, isLimitH && d == up, true)
+		}
+
+		if isNum && !isLimitL && !isLimitH {
+			memo[i][rest][diff] = res
+		}
+
+		return res
+
+		//6366
+		//
+	}
+	return dfs(0, 0, 0, true, true, false)
 }
 
-func dfs(i int, val int, diff int, isLimit bool, isNum bool, k int, s string, memo [][][]int) int {
-
-	if i == len(memo) {
-		if val == 0 && diff == len(memo) && isNum {
-			return 1
-		}
-		return 0
-	}
-	if !isLimit && isNum && memo[i][val][diff] != -1 {
-
-		return memo[i][val][diff]
-	}
-
-	res := 0
-	if !isNum {
-		res = dfs(i+1, 0, diff, false, false, k, s, memo)
-	}
-
-	up := 9
-	if isLimit {
-		up = int(s[i] - '0')
-	}
-	start := 0
-	if !isNum {
-		start = 1
-	}
-	for j := start; j <= up; j++ {
-		res += dfs(i+1, (val*10+j)%k, diff+(j%2)*2-1, isLimit && (j == up), true, k, s, memo)
-	}
-	if !isLimit && isNum {
-		memo[i][val][diff] = res
-	}
-	return res
+func main() {
+	println(numberOfBeautifulIntegers(0, 0, 1))
 }
