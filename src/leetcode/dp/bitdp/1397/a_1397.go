46003bf6e7ba47f31b4c16d3324e517d8b3cb9a8
jian.pan <jian.pan@jiduauto.com>
2024-04-29 11:26:30 +0800

    数位dp+kmp

--- src/leetcode/dp/bitdp/1397/a_1397.go
+++ src/leetcode/dp/bitdp/1397/a_1397.go
@@ -4,6 +4,14 @@ import "strings"
 
 var mod = 1_000_000_007
 
+/*
+*
+给你两个长度为 n 的字符串 s1 和 s2 ，以及一个字符串 evil 。请你返回 好字符串 的数目。
+
+好字符串 的定义为：它的长度为 n ，字典序大于等于 s1 ，字典序小于等于 s2 ，且不包含 evil 为子字符串。
+
+由于答案可能很大，请你返回答案对 10^9 + 7 取余的结果。
+*/
 func findGoodStrings(n int, s1 string, s2 string, evil string) int {
 
 	next := getNext(evil)
@@ -26,6 +34,7 @@ func findGoodStrings(n int, s1 string, s2 string, evil string) int {
 func getNext(evil string) []int {
 
 	n := len(evil)
+	//下一个匹配的位置
 	next := make([]int, n)
 	for i := 1; i < n; i++ {
 		j := next[i-1]
