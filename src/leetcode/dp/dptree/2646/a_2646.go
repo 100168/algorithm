93a40a4df7a88a671ba042ee6fb306bbccc7e07a
jian.pan <jian.pan@jiduauto.com>
2024-04-30 14:36:11 +0800

    还不错

--- src/leetcode/dp/dptree/2646/a_2646.go
+++ src/leetcode/dp/dptree/2646/a_2646.go
@@ -5,45 +5,52 @@ var cnt []int
 func minimumTotalPrice(n int, edges [][]int, price []int, trips [][]int) int {
 
 	g := make([][]int, n)
+
 	for _, v := range edges {
 		x, y := v[0], v[1]
 		g[x] = append(g[x], y)
 		g[y] = append(g[y], x)
 	}
-	cnt = make([]int, n)
-	for _, v := range trips {
-		x, y := v[0], v[1]
-		dfs(x, -1, y, g, price)
-	}
-	return min(minDfs(0, -1, g, price))
-}
 
-func dfs(x, fa, target int, g [][]int, price []int) bool {
-	if x == target {
-		cnt[x]++
-		return true
-	}
+	cnt := make([]int, n)
 
-	for _, y := range g[x] {
-		if y != fa && dfs(y, x, target, g, price) {
+	var find func(int, int, int) bool
+
+	find = func(x, fa, target int) bool {
+		if x == target {
 			cnt[x]++
 			return true
 		}
-	}
-	return false
-}
-
-// 跟打家劫舍3一样
-func minDfs(x, fa int, g [][]int, price []int) (half, all int) {
-	all = cnt[x] * price[x]
-	half = all / 2
-
-	for _, y := range g[x] {
-		if y != fa {
-			h, a := minDfs(y, x, g, price)
-			half += a
-			all += min(h, a)
+		for _, y := range g[x] {
+			if y == fa {
+				continue
+			}
+			if find(y, x, target) {
+				cnt[x]++
+				return true
+			}
 		}
+		return false
 	}
-	return half, all
+
+	for _, v := range trips {
+		find(v[0], -1, v[1])
+	}
+
+	var dfs func(int, int) (int, int)
+	dfs = func(x, fa int) (int, int) {
+		noHalf := cnt[x] * price[x]
+		half := noHalf / 2
+
+		for _, y := range g[x] {
+			if y == fa {
+				continue
+			}
+			s1, s2 := dfs(y, x)
+			half += s2
+			noHalf += min(s1, s2)
+		}
+		return half, noHalf
+	}
+	return min(dfs(0, -1))
 }
