2ee77ca84e642e9cb6747e598075eb1b962b8f3e
jian.pan <jian.pan@jiduauto.com>
2024-05-30 11:04:55 +0800

    难顶

--- src/leetcode/util/NumberUtil.go
+++ src/leetcode/util/NumberUtil.go
@@ -4,19 +4,6 @@ import (
 	"strings"
 )
 
-func MAX(a int, b int) int {
-	if a >= b {
-		return a
-	}
-	return b
-}
-func Min(a int, b int) int {
-	if a >= b {
-		return b
-	}
-	return a
-}
-
 func brace(s string) string {
 	s = strings.Replace(s, "[", "{", len(s))
 	s = strings.Replace(s, "]", "}", len(s))
