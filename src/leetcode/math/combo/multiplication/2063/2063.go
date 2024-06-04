a8ce3255078174b2ee9060ecc01b756c22b2d04d
jian.pan <jian.pan@jiduauto.com>
2024-06-04 19:23:58 +0800

    感觉自己是个智障

--- src/leetcode/math/combo/multiplication/2063/2063.go
+++ src/leetcode/math/combo/multiplication/2063/2063.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 /*
 *
@@ -19,18 +22,13 @@ import "fmt"
 - "aba" 中有 2 个元音
 因此，元音总数 = 0 + 1 + 1 + 1 + 1 + 2 = 6 。
 */
-func countVowels(word string) int64 {
-
-	ans := 0
-	oMap := map[uint8]bool{'a': true, 'e': true, 'i': true, 'o': true, 'u': true}
-
-	cnt := 0
-	for i := 0; i < len(word); i++ {
-		if oMap[word[i]] {
-			cnt++
+func countVowels(word string) (ans int64) {
+	for i, ch := range word {
+		if strings.ContainsRune("aeiou", ch) {
+			ans += int64(i+1) * int64(len(word)-i)
 		}
 	}
-	return int64(ans)
+	return
 }
 
 func main() {
