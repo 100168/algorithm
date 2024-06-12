ae3cceb191204e9572533be7b57c0b584690fd21
jian.pan <jian.pan@jiduauto.com>
2024-06-12 19:23:24 +0800

    组合计数

--- src/leetcode/competition/week/401/c/c.go
+++ src/leetcode/competition/week/401/c/c.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"math/big"
+	"fmt"
+	"math/bits"
 	"slices"
-	"sort"
 )
 
 /*
@@ -18,38 +18,64 @@ import (
 则将 rewardValues[i] 加到 x 上（即 x = x + rewardValues[i]），并 标记 下标 i。
 以整数形式返回执行最优操作能够获得的 最大 总奖励。
 */
-func maxTotalReward(rewardValues []int) int {
 
-	sort.Ints(rewardValues)
-	//这又是什么狗掰
+const w = bits.UintSize
 
-	var dfs func(int, int) bool
+type bitset []uint
 
-	dfs = func(i int, j int) bool {
-
-		if i == 0 && j == 0 {
-			return true
+// b <<= k
+func (b bitset) lsh(k int) bitset {
+	shift, offset := k/w, k%w
+	if offset == 0 {
+		// Fast path
+		copy(b[shift:], b)
+	} else {
+		for i := len(b) - 1; i > shift; i-- {
+			b[i] = b[i-shift]<<offset | b[i-shift-1]>>(w-offset)
 		}
-		cur := dfs(i-1, j)
-		//x+v==j
-		if j-rewardValues[i] < rewardValues[i] && j >= rewardValues[i] {
-			cur = cur || dfs(i-1, j-rewardValues[i])
-		}
-		return cur
+		b[shift] = b[0] << offset
 	}
-	return 1
+	clear(b[:shift])
+	return b
 }
 
-func maxTotalReward2(rewardValues []int) int {
+// 把 >= start 的清零
+func (b bitset) resetRange(start int) bitset {
+	i := start / w
+	b[i] &= ^(^uint(0) << (start % w))
+	clear(b[i+1:])
+	return b
+}
+
+// b |= c
+func (b bitset) unionFrom(c bitset) {
+	for i, v := range c {
+		b[i] |= v
+	}
+}
+
+func (b bitset) lastIndex1() int {
+	for i := len(b) - 1; i >= 0; i-- {
+		if b[i] != 0 {
+			return i*w | (bits.Len(b[i]) - 1)
+		}
+	}
+	return -1
+}
+
+func maxTotalReward(rewardValues []int) int {
 	slices.Sort(rewardValues)
 	rewardValues = slices.Compact(rewardValues) // 去重
 
-	one := big.NewInt(1)
-	f := big.NewInt(1)
-	p := new(big.Int)
+	m := rewardValues[len(rewardValues)-1]
+	f := make(bitset, m*2/w+1)
+	f[0] = 1
 	for _, v := range rewardValues {
-		mask := p.Sub(p.Lsh(one, uint(v)), one)
-		f.Or(f, p.Lsh(p.And(f, mask), uint(v)))
+		f.unionFrom(slices.Clone(f).lsh(v).resetRange(v * 2))
 	}
-	return f.BitLen() - 1
+	return f.lastIndex1()
+}
+
+func main() {
+	fmt.Println(bits.Len(10))
 }
---
+++ src/leetcode/math/combo/count/1643/1643.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+
+/*
+*
+Bob 站在单元格 (0, 0) ，想要前往目的地 destination ：(row, column) 。
+他只能向 右 或向 下 走。你可以为 Bob 提供导航 指令 来帮助他到达目的地 destination 。
+
+指令 用字符串表示，其中每个字符：
+
+'H' ，意味着水平向右移动
+'V' ，意味着竖直向下移动
+能够为 Bob 导航到目的地 destination 的指令可以有多种，例如，如果目的地 destination 是 (2, 3)，"HHHVV" 和 "HVHVH" 都是有效 指令 。
+
+然而，Bob 很挑剔。因为他的幸运数字是 k，他想要遵循 按字典序排列后的第 k 条最小指令 的导航前往目的地 destination 。k  的编号 从 1 开始 。
+
+给你一个整数数组 destination 和一个整数 k ，请你返回可以为 Bob 提供前往目的地 destination 导航的 按字典序排列后的第 k 条最小指令 。
+
+输入：destination = [2,3], k = 1
+输出："HHHVV"
+解释：能前往 (2, 3) 的所有导航指令 按字典序排列后 如下所示：
+["HHHVV", "HHVHV", "HHVVH", "HVHHV", "HVHVH", "HVVHH", "VHHHV", "VHHVH", "VHVHH", "VVHHH"].
+*/
+func kthSmallestPath(destination []int, k int) string {
+	//
+
+	h := destination[0]
+	v := destination[1]
+
+	comb := make([][]int, h+v)
+	for i := range comb {
+		comb[i] = make([]int, h)
+	}
+
+	comb[0][0] = 1
+
+	for i := 1; i < h+v; i++ {
+		comb[i][0] = 1
+		for j := 1; j <= i && j < h; j++ {
+			comb[i][j] = comb[i-1][j-1] + comb[i-1][j]
+		}
+	}
+	ans := ""
+
+	up := h + v
+	for i := 0; i < up; i++ {
+
+		if h > 0 {
+			o := comb[h+v-1][h-1]
+			if k > o {
+				ans += "V"
+				v--
+				k -= o
+			} else {
+				ans += "H"
+				h--
+			}
+		} else {
+			ans += "V"
+			v--
+		}
+	}
+	return ans
+
+}
+
+func main() {
+	fmt.Println(kthSmallestPath([]int{3, 2}, 3))
+}
