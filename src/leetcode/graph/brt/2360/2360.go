0ddc2d6dee7a424c299c7ebeeb74d98391344e6e
jian.pan <jian.pan@jiduauto.com>
2024-06-26 16:34:25 +0800

    时间戳实现太秀了

--- src/leetcode/graph/brt/2360/2360.go
+++ src/leetcode/graph/brt/2360/2360.go
@@ -61,6 +61,32 @@ func longestCycle(edges []int) int {
 
 }
 
+/*
+*
+
+时间戳来实现
+*/
+func longestCycle2(edges []int) int {
+	time := make([]int, len(edges))
+	clock, ans := 1, -1
+	for x, t := range time {
+		if t > 0 {
+			continue
+		}
+		for startTime := clock; x >= 0; x = edges[x] {
+			if time[x] > 0 { // 重复访问
+				if time[x] >= startTime { // 找到了一个新的环
+					ans = max(ans, clock-time[x])
+				}
+				break
+			}
+			time[x] = clock
+			clock++
+		}
+	}
+	return ans
+}
+
 func main() {
 	fmt.Println(longestCycle([]int{-1, 4, -1, 2, 0, 4}))
 }
