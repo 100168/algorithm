450277eb45bb968cf14c53c63a9210751b47ac14
jian.pan <jian.pan@jiduauto.com>
2024-07-08 10:14:49 +0800

    后缀数组先放着

--- src/leetcode/competition/week/405/d/d.go
+++ src/leetcode/competition/week/405/d/d.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"index/suffixarray"
+	"math"
+)
+
 /**
 给你一个字符串 target、一个字符串数组 words 以及一个整数数组 costs，这两个数组长度相同。
 
@@ -14,33 +19,36 @@ package main
 */
 
 func minimumCost(target string, words []string, costs []int) int {
+	minCost := map[string]uint16{}
+	for i, w := range words {
+		c := uint16(costs[i])
+		if minCost[w] == 0 {
+			minCost[w] = c
+		} else {
+			minCost[w] = min(minCost[w], c)
+		}
+	}
 
 	n := len(target)
-
-	wordMap := make(map[string]int)
-
-	maxL := 0
-	for i, w := range words {
-		if _, ok := wordMap[w]; !ok {
-			wordMap[w] = costs[i]
-		} else {
-			wordMap[w] = min(costs[i], wordMap[w])
+	type pair struct{ l, cost uint16 }
+	from := make([][]pair, n+1)
+	sa := suffixarray.New([]byte(target))
+	for w, c := range minCost {
+		for _, l := range sa.Lookup([]byte(w), -1) {
+			r := l + len(w)
+			from[r] = append(from[r], pair{uint16(l), c})
 		}
-		maxL = max(maxL, len(w))
 	}
 
-	dp := make([]int, n)
-
+	f := make([]int, n+1)
 	for i := 1; i <= n; i++ {
-
-		for j := i - 1; j >= 0 && i-j <= maxL; j-- {
-			cur := target[j:i]
-			if _, ok := wordMap[cur]; ok {
-				dp[i] = min(dp[i], dp[i-j]+wordMap[cur])
-			}
+		f[i] = math.MaxInt / 2
+		for _, p := range from[i] {
+			f[i] = min(f[i], f[p.l]+int(p.cost))
 		}
-
 	}
-
-	return dp[n]
+	if f[n] == math.MaxInt/2 {
+		return -1
+	}
+	return f[n]
 }
