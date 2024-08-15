901035943b330a49424035c9572da0fc44eeab39
jian.pan <jian.pan@jiduauto.com>
2024-08-15 16:24:20 +0800

    加练加练

--- src/leetcode/greedy/2835/2835.go
+++ src/leetcode/greedy/2835/2835.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"container/heap"
+	"fmt"
+	"math/bits"
+)
+
 /*
 *给你一个下标从 0 开始的数组 nums ，它包含 非负 整数，且全部为 2 的幂，同时给你一个整数 target 。
 
@@ -28,41 +34,103 @@ package main
 第二次操作中，我们选择元素 nums[3] 。数组变为 nums = [1,1,2,16,8,8] 。
 这时候，nums 包含子序列 [1,1,2,8] ，和为 12 。
 无法通过更少的操作得到和为 12 的子序列。
+1，2，3，4
 */
 func minOperations(nums []int, target int) int {
 
 	s := 0
 
-	ops := 0
-	cntMap := make(map[int]int)
 	for _, v := range nums {
-		if target&v != 1 {
-			target ^= v
-			continue
-		}
-		cntMap[v]++
 		s += v
+	}
+
+	if s < target {
+		return -1
+	}
+
+	s = 0
+	ops := 0
+
+	hp := new(myHeap)
+	for _, v := range nums {
+		heap.Push(hp, v)
+	}
+	mostRight := bits.TrailingZeros(uint(target))
+	s = 0
+	for target > 0 {
+		for s < 1<<mostRight && (*hp)[0] < 1<<mostRight {
+			s += heap.Pop(hp).(int)
+		}
+		if s >= 1<<mostRight {
+			s -= 1 << mostRight
+		} else {
+			pop := heap.Pop(hp).(int)
+			index := bits.TrailingZeros(uint(pop))
+			ops += index - mostRight
+			for index > mostRight {
+				heap.Push(hp, 1<<(index-1))
+				index--
+			}
+		}
+		target &= target - 1
+
+		mostRight = bits.TrailingZeros(uint(target))
+	}
+	return ops
+
+}
+
+type myHeap []int
+
+func (h *myHeap) Less(i, j int) bool {
+	return (*h)[i] < (*h)[j]
+}
+
+func (h *myHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *myHeap) Len() int {
+	return len(*h)
+}
+
+func (h *myHeap) Pop() (v any) {
+	*h, v = (*h)[:h.Len()-1], (*h)[h.Len()-1]
+	return
+}
+
+func (h *myHeap) Push(v any) {
+	*h = append(*h, v.(int))
+}
+
+func minOperations2(nums []int, target int) (ans int) {
+	s := 0
+	cnt := [31]int{}
+	for _, v := range nums {
+		s += v
+		cnt[bits.TrailingZeros(uint(v))]++
 	}
 	if s < target {
 		return -1
 	}
 	s = 0
-
-	targetMap := make(map[int]int)
-
-	for _, v := range nums {
-		if s+v < target {
-			s += v
-			if cntMap[v]--; cntMap[v] == 0 {
-				delete(cntMap, v)
-			}
-		} else {
-			for s+v != target {
-
-			}
-			return ops
+	for i := 0; 1<<i <= target; {
+		s += cnt[i] << i
+		mask := 1<<(i+1) - 1
+		if s >= target&mask {
+			i++
+			continue
+		}
+		ans++
+		for i++; cnt[i] == 0; i++ {
+			ans++
 		}
 	}
-	return -1
+	return
+}
 
+func main() {
+	fmt.Println(minOperations([]int{1, 2, 8}, 7))
+	fmt.Println(minOperations([]int{1024}, 400))
+	fmt.Println(1 << 30)
 }
