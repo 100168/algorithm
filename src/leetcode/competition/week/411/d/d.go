fe7f8b6bb3528cc1e78ec4fcb94bbd3a3228cbc2
jian.pan <jian.pan@jiduauto.com>
2024-09-06 17:56:05 +0800

    太难了

--- src/leetcode/competition/week/411/d/d.go
+++ src/leetcode/competition/week/411/d/d.go
@@ -59,6 +59,40 @@ func countKConstraintSubstrings(s string, k int, queries [][]int) []int64 {
 	return ans
 }
 
+func countKConstraintSubstrings2(s string, k int, queries [][]int) []int64 {
+	n := len(s)
+	left := make([]int, n)
+	sum := make([]int, n+1)
+	cnt := [2]int{}
+	l := 0
+	for i, c := range s {
+		cnt[c&1]++
+		for cnt[0] > k && cnt[1] > k {
+			cnt[s[l]&1]--
+			l++
+		}
+		left[i] = l
+		sum[i+1] = sum[i] + i - l + 1
+	}
+
+	right := make([]int, n)
+	l = 0
+	for i := range right {
+		for l < n && left[l] < i {
+			l++
+		}
+		right[i] = l
+	}
+
+	ans := make([]int64, len(queries))
+	for i, q := range queries {
+		l, r := q[0], q[1]
+		j := min(right[l], r+1)
+		ans[i] = int64(sum[r+1] - sum[j] + (j-l+1)*(j-l)/2)
+	}
+	return ans
+}
+
 func main() {
 	fmt.Println(countKConstraintSubstrings("0001111", 2, [][]int{{0, 6}}))
 }
