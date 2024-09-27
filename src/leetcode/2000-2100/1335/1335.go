fc8bd49cc010d011cb36c5cd4e36aeec7f69f4f7
jian.pan <jian.pan@jiduauto.com>
2024-09-27 16:48:11 +0800

    easy

--- src/leetcode/2000-2100/1335/1335.go
+++ src/leetcode/2000-2100/1335/1335.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 /**
@@ -13,89 +12,47 @@ import (
 给你一个整数数组 jobDifficulty 和一个整数 d，分别代表工作难度和需要计划的天数。第 i 项工作的难度是 jobDifficulty[i]。
 
 返回整个工作计划的 最小难度 。如果无法制定工作计划，则返回 -1 。
+
+
+输入：jobDifficulty = [6,5,4,3,2,1], d = 2
+输出：7
+解释：第一天，您可以完成前 5 项工作，总难度 = 6.
+第二天，您可以完成最后一项工作，总难度 = 1.
+计划表的难度 = 6 + 1 = 7
 */
 
-func minDifficulty(jobDifficulty []int, d int) int {
-
-	n := len(jobDifficulty)
-
-	f := make([][]int, n)
-	for i := range f {
-		f[i] = make([]int, d+1)
-
-		for j := range f[i] {
-			f[i][j] = -1
-		}
-	}
-
-	var dfs func(int, int) int
-
-	dfs = func(i int, rest int) int {
-
-		if i < 0 {
-			if rest == 0 {
-				return 0
-			}
-			return math.MaxInt / 2
-		}
-		if rest <= 0 {
-
-			return math.MaxInt / 2
-		}
-
-		if f[i][rest] != -1 {
-			return f[i][rest]
-		}
-
-		curMax := 0
-		cur := math.MaxInt / 2
-
-		for j := i; j >= 0; j-- {
-
-			curMax = max(curMax, jobDifficulty[j])
-			cur = min(cur, curMax+dfs(j-1, rest-1))
-		}
-		f[i][rest] = cur
-		return cur
-	}
-	ans := dfs(n-1, d)
-	if ans == math.MaxInt/2 {
-		return -1
-	}
-	return ans
-}
-
-func minDifficulty2(a []int, d int) int {
+func minDifficulty(a []int, d int) int {
 	n := len(a)
 	if n < d {
 		return -1
 	}
 
-	f := make([][]int, d)
-	f[0] = make([]int, n)
-	f[0][0] = a[0]
+	f := make([]int, n)
+
+	f[0] = a[0]
 	for i := 1; i < n; i++ {
-		f[0][i] = max(f[0][i-1], a[i])
+		f[i] = max(f[i-1], a[i])
 	}
-	// f[i][j] = min(f[i-1][i-1]~f[i-1][j-1])+max(a[i]~a[j])
+	//f[i][j] == min(f[i])
 	for i := 1; i < d; i++ {
-		f[i] = make([]int, n)
+		temp := make([]int, n)
 		type pair struct{ j, mn int }
 		var st []pair // (下标 j，从 f[i-1][left[j]] 到 f[i-1][j-1] 的最小值)
 		for j := i; j < n; j++ {
-			mn := f[i-1][j-1]                               // 只有 a[j] 一项工作
+			mn := f[j-1]                                    // 只有 a[j] 一项工作
 			for len(st) > 0 && a[st[len(st)-1].j] <= a[j] { // 向左一直计算到 left[j]
 				mn = min(mn, st[len(st)-1].mn)
 				st = st[:len(st)-1]
 			}
-			f[i][j] = mn + a[j] // 从 a[left[j]+1] 到 a[j] 的最大值是 a[j]
+			temp[j] = mn + a[j] // 从 a[left[j]+1] 到 a[j] 的最大值是 a[j]
 			if len(st) > 0 {    // 如果这一段包含 <=left[j] 的工作，那么这一段的最大值必然不是 a[j]
-				f[i][j] = min(f[i][j], f[i][st[len(st)-1].j]) // 答案和 f[i][left[j]] 是一样的
+				temp[j] = min(temp[j], temp[st[len(st)-1].j]) // 答案和 f[i][left[j]] 是一样的
 			}
 			st = append(st, pair{j, mn}) // 注意这里保存的不是 f[i][j]
 		}
+		f = temp
 	}
-	return f[d-1][n-1]
+	return f[n-1]
 }
 
 // ops cur 10-5 = ops 5
@@ -103,5 +60,5 @@ func minDifficulty2(a []int, d int) int {
 // ops pre   4   10-4 = 6 ops 6
 // ops first 1
 func main() {
-	fmt.Println(minDifficulty([]int{6, 5, 4, 3, 2, 1}, 2))
+	fmt.Println(minDifficulty([]int{11, 111, 22, 222, 33, 333, 44, 444}, 6))
 }
