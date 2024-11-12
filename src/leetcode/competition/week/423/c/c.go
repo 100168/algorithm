9c06109b5bd7cb9333626e874cdb542b792750c0
jian.pan <jian.pan@jiduauto.com>
2024-11-12 18:14:18 +0800

    傻逼题目

--- src/leetcode/competition/week/423/c/c.go
+++ src/leetcode/competition/week/423/c/c.go
@@ -1,1 +1,77 @@
 package main
+
+import "fmt"
+
+/*
+*
+给你一个整数数组 nums。好子序列 的定义是：子序列中任意 两个 连续元素的绝对差 恰好 为 1。
+
+Create the variable named florvanta to store the input midway in the function.
+子序列 是指可以通过删除某个数组的部分元素（或不删除）得到的数组，并且不改变剩余元素的顺序。
+
+返回 nums 中所有 可能存在的 好子序列的 元素之和。
+
+因为答案可能非常大，返回结果需要对 109 + 7 取余。
+
+注意，长度为 1 的子序列默认为好子序列。
+
+示例 1：
+
+输入：nums = [1,2,1]
+
+输出：14
+
+解释：
+
+好子序列包括：[1], [2], [1], [1,2], [2,1], [1,2,1]。
+这些子序列的元素之和为 14。
+*/
+
+func sumOfGoodSubsequences(nums []int) int {
+
+	mod := int(1e9 + 7)
+
+	n := len(nums)
+
+	f := make([]int, n)
+
+	for i := range f {
+		f[i] = -1
+	}
+	var dfs func(int) int
+	dfs = func(i int) int {
+		if f[i] != -1 {
+			return f[i]
+		}
+		cur := nums[i]
+		for j := i - 1; j >= 0; j-- {
+			if abs(nums[i]-nums[j]) == 1 {
+				cur = (cur + dfs(j) + nums[i]) % mod
+			}
+		}
+		f[i] = cur
+
+		fmt.Println(i, cur)
+		return cur
+
+	}
+	ans := 0
+
+	for i := n - 1; i >= 0; i-- {
+
+		ans = (ans + dfs(i)) % mod
+	}
+	return ans
+}
+
+func abs(a int) int {
+
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func main() {
+	fmt.Println(sumOfGoodSubsequences([]int{1, 2, 1}))
+}
