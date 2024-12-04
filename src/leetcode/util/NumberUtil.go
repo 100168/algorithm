b50c86f90f4fdc98888b1aff9f3283fd7654f956
jian.pan <jian.pan@jiduauto.com>
2024-12-04 17:47:49 +0800

    加油吧

--- src/leetcode/util/NumberUtil.go
+++ src/leetcode/util/NumberUtil.go
@@ -10,7 +10,54 @@ func brace(s string) string {
 	s = strings.Replace(s, "]", "}", len(s))
 	return s
 }
+
+func join(s string) string {
+
+	split := strings.Split(s, ",")
+
+	for i := range split {
+
+		split[i] = "'" + split[i] + "'"
+	}
+
+	return strings.Join(split, ",")
+
+}
+
+func diff(s1, s2 string) string {
+
+	sp1 := strings.Split(s1, ",")
+	sp2 := strings.Split(s2, ",")
+
+	m1 := make(map[string]bool)
+	for _, v := range sp1 {
+
+		m1[v] = true
+	}
+
+	set := make(map[string]bool)
+	ans := make([]string, 0)
+
+	for _, v := range sp2 {
+
+		if set[v] {
+			continue
+		}
+		set[v] = true
+		if !m1[v] {
+			ans = append(ans, v)
+		}
+	}
+
+	return strings.Join(ans, ",")
+
+}
 func main() {
 
-	fmt.Println(brace("[[0,0,2],[0,1,5],[2,2,5],[0,2,4]]"))
+	s := 13
+
+	for sub := s; sub != 0; sub = s & (sub - 1) {
+		fmt.Println(sub)
+	}
+
 }
